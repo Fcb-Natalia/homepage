89be9ffc021f16a7012d3ec8de3def8ea5978ef1
Natalia Romaszko <natalia.romaszko@gmail.com>
2022-11-29 11:06:11 +0100

    name change

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 console.log("Witam wszystkich na mojej stronie :)")
 
-let body__changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
+let changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
 let body = document.querySelector(".body");
-let body__nextColorButton = document.querySelector(".js-nextColorButton");
+let nextColorButton = document.querySelector(".js-nextColorButton");
 
 
-body__changeBackgroundButton.addEventListener("click", () => {
+changeBackgroundButton.addEventListener("click", () => {
     body.classList.toggle("body--lightRed");
 
-    body__nextColorButton.innerText = body.classList.contains("body--lightRed") ? "jasny" : "czerwony";
+    nextColorButton.innerText = body.classList.contains("body--lightRed") ? "jasny" : "czerwony";
 })
 
 
